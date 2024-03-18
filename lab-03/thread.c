4d28b4743fdc1321cdddbb34120dac7331fb5a97
Cloud User <ec2-user@ip-172-31-28-22.ec2.internal>
2024-03-18 00:46:32 +0000

    update thread.c

--- lab-03/thread.c
+++ lab-03/thread.c
@@ -1,17 +1,16 @@
-#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
 
-// The child thread will execute this function
+// Funcao a ser executada pelo child
 void* threadFunction(void* argument) {
     char* childName = (char*)argument;
     
-    // receiving parent string
+    // Recebendo a string do parent
     printf("Child receiving string: %s\n", childName);
 
-    // changing the received string
+    // Mudando a string recebida
     strcpy(childName, "Cleiton");
     printf("Child exiting...%s\n", childName);
 
