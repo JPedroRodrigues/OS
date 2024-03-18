3d4f1cb581f75de512be6aabb6ec1977f5cef04b
Cloud User <ec2-user@ip-172-31-28-22.ec2.internal>
2024-03-18 00:29:48 +0000

    add thread exercise

--- lab-03/thread.c
+++ lab-03/thread.c
@@ -1,37 +1,31 @@
 #define _GNU_SOURCE
-#include <stdlib.h> 
-#include <sys/types.h> 
-#include <sys/wait.h>
-#include <signal.h>
-#include <sched.h>
+#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <pthread.h>
 #include <string.h>
+#include <pthread.h>
 
 // The child thread will execute this function
-void* threadFunction( void* argument ) {
-	// receiving parent name
-        char *childName = (char *) argument;
-        printf("Child receiving string: %s\n", childName);
-       
-	strcpy(childName, "Cleiton");
-        printf("Child exiting...%s\n", childName);             
-} 
- 
-
-int main() { 
-        pthread_t thread_id;
-
-        char *name = "Klaythom";
-        printf("Parent name: %s\n", name);
-
-        // create thread
-        pthread_create(&thread_id, NULL, threadFunction, (void *) &name);
-
-        // waiting thread resolve
-        pthread_join(thread_id, NULL);
-
-        return 0;
+void* threadFunction(void* argument) {
+    char* childName = (char*)argument;
+    printf("Child receiving string: %s\n", childName);
+    strcpy(childName, "Cleiton");
+    printf("Child exiting...%s\n", childName);
+    return NULL;
 }
 
+int main() {
+    pthread_t thread_id;
+
+    char name[] = "Klaythom"; // Modified to an array to allow modification
+    printf("Parent name: %s\n", name);
+
+    // Create thread
+    pthread_create(&thread_id, NULL, threadFunction, (void*)name);
+
+    // Wait for the thread to finish
+    pthread_join(thread_id, NULL);
+
+    printf("Parent name after thread: %s\n", name);
+
+    return 0;
+}
