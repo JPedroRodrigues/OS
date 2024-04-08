f9d96fa2d181db1b901c9f917dc041ece259aa6d
Joao Vieira <jpedrovieira34@gmail.com>
2024-04-07 22:25:35 -0300

    add a thread-base solution

--- projeto-01/escalator-thread.c
+++ projeto-01/escalator-thread.c
@@ -2,6 +2,8 @@
 #include <pthread.h>
 #define MAXN 10000
 
+int out = 0;
+
 typedef struct {
     int time;
     int direction;
@@ -9,66 +11,45 @@ typedef struct {
 
 
 typedef struct {
-    Person person;
-    Person queue[MAXN];
-    int i;
+    Person fQueue[MAXN];
+    int lFQ;
+    Person sQueue[MAXN];
+    int lSQ;
 } ThreadArgs;
 
 
-void* setFirst(void * threadArgs) {
-    ThreadArgs* args = (ThreadArgs *) threadArgs;
-
-    args->person = args->queue[args->i];
-    pthread_exit(NULL);
-}
-
-void* setOut(void* out) {
-    
-}
-
-int escalator(Person firstQueue[], int lenFQ, Person secondQueue[], int lenSQ) {
-    phtread_t threadID;
-
+void* escalator(void* args) {
+    ThreadArgs* eArgs = (ThreadArgs *) args;
     Person first;
 
     // Setting the first value;
-    if (lenFQ == 0) first = secondQueue[0];
-    else if (lenSQ == 0) first = firstQueue[0];
-    else first = firstQueue[0].time < secondQueue[0].time ? firstQueue[0] : secondQueue[0];
+    if (eArgs->lFQ == 0) first = eArgs->sQueue[0];
+    else if (eArgs->lSQ == 0) first = eArgs->fQueue[0];
+    else first = eArgs->fQueue[0].time < eArgs->sQueue[0].time ? eArgs->fQueue[0] : eArgs->sQueue[0];
 
-    int aux1 = 0, aux2 = 0, out = 0;
+    int aux1 = 0, aux2 = 0;
 
-    while (aux1 < lenFQ || aux2 < lenSQ) {
+    while (aux1 < eArgs->lFQ || aux2 < eArgs->lSQ) {
         
         // going from left to right
         if (first.direction == 0) {
 
             // follows the same direction if the time of arrival is less than the last exit time or it's immediately next to the previous one
-            if ((aux1 < lenFQ && (firstQueue[aux1].time <= out) || (firstQueue[aux1].time > out && firstQueue[aux1].time < secondQueue[aux2].time)) || aux2 == lenSQ) {
-                ThreadArgs args;
-                args.person = first;
-
-                for (int i = 0; i < lenFQ; i++) args.queue[i] = firstQueue[i];
-                
-                args.i = aux1;
-
-                pthread_create(&threadID, NULL, setFirst, (void *) &args);
-                pthread_join(thread_id, NULL);
-                
-                first = args.person;               
+            if ((aux1 < eArgs->lFQ && (eArgs->fQueue[aux1].time <= out) || (eArgs->fQueue[aux1].time > out && eArgs->fQueue[aux1].time < eArgs->sQueue[aux2].time)) || aux2 == eArgs->lSQ) {
+                first = eArgs->fQueue[aux1];               
                 ++aux1;   
 
             // if the time of arrival is greater than the exit time or the queue is completely used 
-            } else if (firstQueue[aux1].time > out || aux1 == lenFQ) {
+            } else if (eArgs->fQueue[aux1].time > out || aux1 == eArgs->lFQ) {
                 // change the escalator's direction, considering a new time of arrival based on the waiting time
-                first = secondQueue[aux2];
+                first = eArgs->sQueue[aux2];
                 if (out > first.time) first.time = out;
                 ++aux2;
 
                 // if the exit time is greater than the following ones
                 int i = aux2;
-                while (out > secondQueue[i].time && i < lenSQ) {
-                    secondQueue[i].time = out;
+                while (out > eArgs->sQueue[i].time && i < eArgs->lSQ) {
+                    eArgs->sQueue[i].time = out;
                     ++i;
                 }
             } 
@@ -76,32 +57,34 @@ int escalator(Person firstQueue[], int lenFQ, Person secondQueue[], int lenSQ) {
         } else if (first.direction == 1) {
             
             // follows the same direction if the time of arrival is less than the last exit time or it's immediately next to the previous one
-            if ((aux2 < lenSQ && secondQueue[aux2].time <= out || (secondQueue[aux2].time > out && secondQueue[aux2].time < firstQueue[aux1].time)) || aux1 == lenFQ) {
-                first = secondQueue[aux2];      
+            if ((aux2 < eArgs->lSQ && eArgs->sQueue[aux2].time <= out || (eArgs->sQueue[aux2].time > out && eArgs->sQueue[aux2].time < eArgs->fQueue[aux1].time)) || aux1 == eArgs->lFQ) {
+                first = eArgs->sQueue[aux2];      
                 ++aux2;
 
             // if the time of arrival is greater than the exit time or the queue is completely used                                               
-            } else if (secondQueue[aux2].time > out || aux2 == lenSQ) {
+            } else if (eArgs->sQueue[aux2].time > out || aux2 == eArgs->lSQ) {
                 // change the escalator's direction, considering a new time of arrival based on the waiting time
-                first = firstQueue[aux1];
+                first = eArgs->fQueue[aux1];
                 if (out > first.time) first.time = out;
                 ++aux1;
 
                 // if the exit time is greater than the following ones
                 int i = aux1;
-                while (out > firstQueue[i].time && i < lenFQ) {
-                    firstQueue[i].time = out;
+                while (out > eArgs->fQueue[i].time && i < eArgs->lFQ) {
+                    eArgs->fQueue[i].time = out;
                     ++i;
                 }                
             }
         }   
         out = first.time + 10;
     }
-    return out;
+    return NULL;
 }
 
 
 int main() {
+    pthread_t thread_id;
+
     // n = number of people (cases); t = time of arrival; d = direction (0: left to right; 1: right to left)
     int n;
     Person person;
@@ -111,10 +94,8 @@ int main() {
     Person secondQueue[MAXN];
 
     /**
-     * lenFQ = length of firstQueue queue
-     * lenSQ = length of secondQueue queue
-     * first = first person to enter the escalator
-     * direction = the direction in which the first person is going 
+     * lenFQ = length of fQueue queue
+     * lenSQ = length of sQueue queue
     */
     int lenFQ = 0, lenSQ = 0;
 
@@ -125,14 +106,21 @@ int main() {
         else secondQueue[lenSQ++] = person;        
     }
 
-    // 2nd example
-        // firstQueue = {5, 9};
-        // secondQueue = {7};
-    // 3rd example
-        // firstQueue = {5, 16};
-        // secondQueue = {10};
+    // Setting up the args to thread
+    ThreadArgs threadArgs;
 
-    int out = escalator(firstQueue, lenFQ, secondQueue, lenSQ);
+    for (int i = 0; i < lenFQ; i++) threadArgs.fQueue[i] = firstQueue[i];
+
+    threadArgs.lFQ = lenFQ;
+
+    for (int i = 0; i < lenSQ; i++) threadArgs.sQueue[i] = secondQueue[i];
+
+    threadArgs.lSQ = lenSQ;
+
+    pthread_create(&thread_id, NULL, escalator, (void *) &threadArgs);
+
+    pthread_join(thread_id, NULL);
+
     printf("Out: %d\n", out);
 
     return 0;
